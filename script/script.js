7bceedc6f1a60a5b74615d5536a5510ee4d3948e
Denis Solovyev <solovyev.denis@gmail.com>
2022-11-11 12:51:36 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -5,29 +5,32 @@ let popupCloseBtn = document.querySelector('.popup__btn-close');
 let popupSaveBtn = document.querySelector('.popup__btn-submit');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let inputName = document.querySelector('.popup__input-name');
-let inputDescription = document.querySelector('.popup__input-description');
+let formInputName = document.querySelector('.popup__input-name');
+let formInputDescription = document.querySelector('.popup__input-description');
+let formElement = document.querySelector('.form');
 
 function popupOpen () {
     popup.classList.add('popup_opened');
-    inputName.value = profileName.textContent;
-    inputDescription.value = profileDescription.textContent;
+    formInputName.value = profileName.textContent;
+    formInputDescription.value = profileDescription.textContent;
 }
 
 function popupClose () {
     popup.classList.remove('popup_opened');
 }
 
-function popupSave () {
-    profileName.textContent = inputName.value;
-    profileDescription.textContent = inputDescription.value;
+function popupSave (evt) {
+    evt.preventDefault();
+    profileName.textContent = formInputName.value;
+    profileDescription.textContent = formInputDescription.value;
+    popupClose();
 }
 
 profileEditBtn.addEventListener('click',
     () => {
         popupOpen();
     }
-)
+);
 
 popupCloseBtn.addEventListener('click',
     () => {
@@ -35,8 +38,4 @@ popupCloseBtn.addEventListener('click',
     }    
 );
 
-popupSaveBtn.addEventListener('click',
-    () => {
-        popupSave();
-    }
-);
+formElement.addEventListener('submit', popupSave());
